8f33eb69f1724548d5c1e1ac19ea4a73ae47a7a5
Nikita <cold17748@gmail.com>
2026-04-27 19:54:17 +0300

    Add headers and source files for implementing functions in x86 assembly

---
+++ core/include/cpu_flags.h
@@ -0,0 +1,14 @@
+#ifndef CPU_FLAGS_H
+#define CPU_FLAGS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+bool avx2_supported();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ core/include/math_kernels.h
@@ -0,0 +1,16 @@
+#ifndef MATH_KERNELS_H
+#define MATH_KERNELS_H
+
+#include <stddef.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void exp_avx2_aligned(float* operand, float* result, size_t n);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
